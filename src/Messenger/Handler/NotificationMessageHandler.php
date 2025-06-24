46136d17303e17729c8560c624171f5804534632
Rokas Reizgys <rokas.student@gmail.com>
2025-06-24 21:39:10 +0300

    handler and service code

---
+++ src/Messenger/Handler/NotificationMessageHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Messenger\Handler;
+
+use App\Messenger\NotificationMessage;
+use App\Service\NotificationService;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class NotificationMessageHandler
+{
+    public function __construct(private NotificationService $service)
+    {
+    }
+
+    public function __invoke(NotificationMessage $message): void
+    {
+        $this->service->notify($message);
+    }
+}
---
+++ src/Service/NotificationService.php
@@ -0,0 +1,31 @@
+<?php
+// src/Service/NotificationService.php
+namespace App\Service;
+
+use App\Messenger\NotificationMessage;
+use App\Service\Provider\NotifierInterface;
+
+class NotificationService
+{
+    /** @var iterable<NotifierInterface> */
+    public function __construct(private iterable $providers)
+    {
+    }
+
+    public function notify(NotificationMessage $msg): void
+    {
+        foreach ($msg->getChannels() as $channel) {
+            foreach ($this->providers as $p) {
+                if (!$p->supports($channel)) {
+                    continue;
+                }
+                try {
+                    $p->send($msg);
+                    break;
+                } catch (\Throwable $e) {
+                    // will log a problem and try another provider
+                }
+            }
+        }
+    }
+}
